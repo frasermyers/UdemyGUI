84356a0d3e6a756fc33bcc7d0d5a77197c3564e3
Fraser <frasermyersmusic@gmail.com>
2019-07-16 17:05:11 +1000

    23. Canvas Fill

--- Udemy GUI.py
+++ Udemy GUI.py
@@ -4,8 +4,14 @@ root = Tk()
 
 canvas = Canvas(root, width=300, height=300)
 canvas.pack()
-canvas.create_rectangle(20, 20, 100, 270)
-canvas.create_line(20, 20, 100, 270)
-canvas.create_polygon(0, 0, 5, 20, 50, 30, 40, 15)
+
+
+def create_rect(x1, y1, x2, y2):
+    canvas.create_rectangle(x1, y1, x2, y2, fill="blue")
+
+
+create_rect(5, 50, 200, 70)
+create_rect(5, 5, 240, 200)
+
 
 root.mainloop()
