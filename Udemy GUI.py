f42ad4a25be2c8e13c6d6e786f3d8bbfdc4ffe66
Fraser <frasermyersmusic@gmail.com>
2019-07-16 12:18:47 +1000

    3. Labels

--- Udemy GUI.py
+++ Udemy GUI.py
@@ -2,4 +2,10 @@ from tkinter import *
 
 root = Tk()
 
+the_label = Label(root, text="This is our Tkinter Window")
+the_label.pack()
+
+the_labell = Label(root, text="This is our second sentence")
+the_labell.pack()
+
 root.mainloop()
