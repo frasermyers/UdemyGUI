c32c0f1afdd4f4799975828fc43e74eb06e07183
Fraser <frasermyersmusic@gmail.com>
2019-07-16 14:27:38 +1000

    12. Binding Functions

--- Udemy GUI.py
+++ Udemy GUI.py
@@ -3,11 +3,17 @@ from tkinter import *
 root = Tk()
 
 
-def print_name():
+def print_name(event):
     print("Hello there Fraser Myers")
 
 
-button_1 = Button(root, text="Click Me", command=print_name)
+def rude_name(event):
+    print("Cunt")
+
+
+button_1 = Button(root, text="Click Me")
+button_1.bind("<Button-1>", print_name)
+button_1.bind("<Button-3>", rude_name)
 button_1.pack()
 
 root.mainloop()
