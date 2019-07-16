6b5d25d8260cb74375ceb9fa9e2398642d75643e
Fraser <frasermyersmusic@gmail.com>
2019-07-16 16:04:18 +1000

    19. Calculator Part 3

--- Udemy GUI.py
+++ Udemy GUI.py
@@ -8,7 +8,7 @@ equation = StringVar()
 
 calculation = Label(root, textvariable=equation)
 
-equation.set("23+54")
+equation.set("0")
 
 calculation.grid(columnspan=4)
 
@@ -20,11 +20,35 @@ def btn_press(num):
 
 
 button_0 = Button(root, text="0", command=lambda: btn_press(0))
-button_0.grid(row=1, column=0)
+button_0.grid(row=4, column=2)
 button_1 = Button(root, text="1", command=lambda: btn_press(1))
-button_1.grid(row=1, column=1)
+button_1.grid(row=1, column=0)
 button_2 = Button(root, text="2", command=lambda: btn_press(2))
-button_2.grid(row=1, column=2)
+button_2.grid(row=1, column=1)
+button_3 = Button(root, text="3", command=lambda: btn_press(3))
+button_3.grid(row=1, column=2)
+button_4 = Button(root, text="4", command=lambda: btn_press(4))
+button_4.grid(row=2, column=0)
+button_5 = Button(root, text="5", command=lambda: btn_press(5))
+button_5.grid(row=2, column=1)
+button_6 = Button(root, text="6", command=lambda: btn_press(6))
+button_6.grid(row=2, column=2)
+button_7 = Button(root, text="7", command=lambda: btn_press(7))
+button_7.grid(row=3, column=0)
+button_8 = Button(root, text="8", command=lambda: btn_press(8))
+button_8.grid(row=3, column=1)
+button_9 = Button(root, text="9", command=lambda: btn_press(9))
+button_9.grid(row=3, column=2)
+
+plus = Button(root, text="+", command=lambda: btn_press("+"))
+plus.grid(row=1, column=3)
+minus = Button(root, text="-", command=lambda: btn_press("-"))
+minus.grid(row=2, column=3)
+multiply = Button(root, text="*", command=lambda: btn_press("*"))
+multiply.grid(row=3, column=3)
+divide = Button(root, text="/", command=lambda: btn_press("/"))
+divide.grid(row=4, column=3)
+
 
 
 
