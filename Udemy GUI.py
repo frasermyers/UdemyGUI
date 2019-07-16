9dedb1050077c6d565eb3149a1eea5c1d7a89381
Fraser <frasermyersmusic@gmail.com>
2019-07-16 14:23:14 +1000

    11. Button Click Action

--- Udemy GUI.py
+++ Udemy GUI.py
@@ -2,19 +2,12 @@ from tkinter import *
 
 root = Tk()
 
-label_1 = Label(root, text="Name: ")
-label_1.grid(row=0, column=0, sticky=E)
 
-label_2 = Label(root, text="Password: ")
-label_2.grid(row=1, column=0, sticky=E)
+def print_name():
+    print("Hello there Fraser Myers")
 
-entry_space = Entry(root)
-entry_space.grid(row=0, column=1)
 
-entry_space_2 = Entry(root)
-entry_space_2.grid(row=1, column=1)
-
-check_button = Checkbutton(root, text="Remember Password")
-check_button.grid(columnspan=2)
+button_1 = Button(root, text="Click Me", command=print_name)
+button_1.pack()
 
 root.mainloop()
