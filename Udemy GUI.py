b0ee6499f162a36f06d2afc7d32fac42b8f5aba4
Fraser <frasermyersmusic@gmail.com>
2019-07-16 12:56:33 +1000

    8. Entries

--- Udemy GUI.py
+++ Udemy GUI.py
@@ -2,13 +2,10 @@ from tkinter import *
 
 root = Tk()
 
-label_1 = Label(root, text="Label 1")
+label_1 = Label(root, text="Name: ")
 label_1.grid(row=0, column=0)
 
-label_2 = Label(root, text="Label 2")
-label_2.grid(row=0, column=1)
-
-label_3 = Label(root, text="Label 3")
-label_3.grid(row=1, column=0)
+entry_space = Entry(root)
+entry_space.grid(row=0, column=1)
 
 root.mainloop()
