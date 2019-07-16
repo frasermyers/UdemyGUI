6df51603d0a2980fe4d479345b2b83921a9762a7
Fraser <frasermyersmusic@gmail.com>
2019-07-16 17:28:00 +1000

    26. Animations

--- Udemy GUI.py
+++ Udemy GUI.py
@@ -1,13 +1,15 @@
 from tkinter import *
-import random
+import time
 
 root = Tk()
 
 canvas = Canvas(root, width=300, height=300)
 canvas.pack()
 
-
-canvas.create_text(150, 200, text="This is gay", fill='blue', font=("Times", 30))
-
+canvas.create_polygon(10, 10, 10, 60, 50, 35)
+for i in range(0, 60):
+    canvas.move(1, 5, 0)
+    root.update()
+    time.sleep(0.1)
 
 root.mainloop()
