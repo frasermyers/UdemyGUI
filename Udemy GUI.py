a25443a498aef416e4d9ea525a4039cb10da8c46
Fraser <frasermyersmusic@gmail.com>
2019-07-16 13:00:33 +1000

    9. Check Buttons

--- Udemy GUI.py
+++ Udemy GUI.py
@@ -8,4 +8,7 @@ label_1.grid(row=0, column=0)
 entry_space = Entry(root)
 entry_space.grid(row=0, column=1)
 
+check_button = Checkbutton(root, text="Remember Name")
+check_button.grid(columnspan=2)
+
 root.mainloop()
