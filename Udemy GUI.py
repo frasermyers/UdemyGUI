c8ba9f49beca1afda0254220563c65c250024983
Fraser <frasermyersmusic@gmail.com>
2019-07-16 15:33:24 +1000

    17. Calculator Part 1

--- Udemy GUI.py
+++ Udemy GUI.py
@@ -1,27 +1,14 @@
 from tkinter import *
-import tkinter.messagebox
 
 root = Tk()
 
-def random():
-    print("This is a statement")
+equation = StringVar()
 
+calculation = Label(root, textvariable=equation)
 
-main_menu = Menu(root)
-root.configure(menu=main_menu)
+equation.set("23+54")
 
-sub_menu_1 = Menu(main_menu)
-sub_menu_2 = Menu(main_menu)
-
-
-main_menu.add_cascade(label="File", menu=sub_menu_1)
-
-sub_menu_1.add_command(label="Random Function", command=random)
-sub_menu_1.add_command(label="New File", command=random)
-sub_menu_1.add_separator()
-sub_menu_1.add_command(label="Poo", command=random)
-
-main_menu.add_cascade(label="Edit", menu=sub_menu_2)
+calculation.grid(columnspan=4)
 
 
 root.mainloop()
