5f6135e13c647aa9fcb465c3a72d3796d5810dc4
Fraser <frasermyersmusic@gmail.com>
2019-07-16 17:21:33 +1000

    25. Text and Arcs

--- Udemy GUI.py
+++ Udemy GUI.py
@@ -7,15 +7,7 @@ canvas = Canvas(root, width=300, height=300)
 canvas.pack()
 
 
-def random_rects(num):
-    for i in range(0, num):
-        x1 = random.randrange(150)
-        y1 = random.randrange(150)
-        x2 = x1 + random.randrange(150)
-        y2 = y1 + random.randrange(150)
-        canvas.create_rectangle(x1, y1, x2, y2)
-
-random_rects(150)
+canvas.create_text(150, 200, text="This is gay", fill='blue', font=("Times", 30))
 
 
 root.mainloop()
