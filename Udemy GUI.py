d3c86fc2ddeebcb68905d617d7312e27f83979ab
Fraser <frasermyersmusic@gmail.com>
2019-07-16 17:00:00 +1000

    22. Canvas Objects

--- Udemy GUI.py
+++ Udemy GUI.py
@@ -5,5 +5,7 @@ root = Tk()
 canvas = Canvas(root, width=300, height=300)
 canvas.pack()
 canvas.create_rectangle(20, 20, 100, 270)
+canvas.create_line(20, 20, 100, 270)
+canvas.create_polygon(0, 0, 5, 20, 50, 30, 40, 15)
 
 root.mainloop()
